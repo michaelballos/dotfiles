6e7c71a64ae7fab390d4ccd74b9eec330c5100fb
michaelballos <ballos.michael@gmail.com>
2022-09-05 03:17:12 -0500

    Add terminal switch keymaps and diagnostics

--- .config/nvim/lua/michaelballos/maps.lua
+++ .config/nvim/lua/michaelballos/maps.lua
@@ -1,16 +1,22 @@
 -- vim key mappings
 local map = require('utils').map
 
--- leader key is comma
+-- leader key
 vim.g.mapleader = ';'
 
 -- format
-map('n', '<leader>.', '<cmd>lua vim.lsp.buf.formatting()<CR>', { noremap = true, silent = true })
+map('n', '<leader>;', '<cmd>lua vim.lsp.buf.formatting()<CR>', { noremap = true, silent = true })
 
--- switch panes
-map('n', '<leader>j', '<C-w>j', { silent = true })
-map('n', '<leader>k', '<C-w>k', { silent = true })
-map('n', '<leader>h', '<C-w>h', { silent = true })
-map('n', '<leader>l', '<C-w>l', { silent = true })
+-- switch terminal panes
+map('n', '<C-j>', '<C-w>j', { silent = true })
+map('n', '<C-k>', '<C-w>k', { silent = true })
+map('n', '<C-h>', '<C-w>h', { silent = true })
+map('n', '<C-l>', '<C-w>l', { silent = true })
+
+-- vim diagnostics lsp
+map('n', '<space>e', vim.diagnostic.open_float, { silent = true })
+map('n', '[d', vim.diagnostic.goto_prev, { silent = true })
+map('n', ']d', vim.diagnostic.goto_next, { silent = true })
+map('n', '<space>q', vim.diagnostic.setloclist, { silent = true })
 
 
