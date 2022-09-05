1cdf7f7da7223218a2cdde245b18b3a14b662de0
michaelballos <ballos.michael@gmail.com>
2022-09-05 03:16:24 -0500

    No clue

--- .config/nvim/after/plugin/mason.rc.lua
+++ .config/nvim/after/plugin/mason.rc.lua
@@ -75,7 +75,7 @@ local MASON_DEFAULT_SETTINGS = {
 local MASONLSP_DEFAULT_SETTINGS = {
   -- A list of servers to automatically install if they're not already installed. Example: { "rust_analyzer@nightly", "sumneko_lua" }
   -- This setting has no relation with the `automatic_installation` setting.
-  ensure_installed = { 
+  ensure_installed = {
     'tsserver',
     'dockerls',
     'graphql',
