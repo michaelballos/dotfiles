c7a5ba6de7ea277a7dcc057c64212fc05f77e329
michaelballos <ballos.michael@gmail.com>
2022-09-05 04:51:32 -0500

    Add some more keybinds

--- .config/nvim/after/plugin/lspsaga.rc.lua
+++ .config/nvim/after/plugin/lspsaga.rc.lua
@@ -1,22 +1,8 @@
 local present, lspsaga = pcall(require, 'lspsaga')
-if (not present) then 
-  return 
+if (not present) then
+  return
 end
 
 lspsaga.init_lsp_saga {
   server_filetype_map = {}
 }
-
-local opts = { 
-  noremap = true, 
-  silent = true 
-}
-
-local map = vim.keymap.set
-
-map('n', '<C-j>', '<Cmd>Lspsaga diagnostic_jump_next<cr>', opts)
-map('n', 'K', '<Cmd>Lspsaga hover_doc<cr>', opts)
-map('n', 'gd', '<Cmd>Lspsaga lsp_finder<cr>', opts)
-map('i', '<C-k>', '<Cmd>Lspsaga signature_help<cr>', opts)
-map('n', 'gp', '<Cmd>Lspsaga preview_definition<cr>', opts)
-map('n', 'gr', '<Cmd>Lspsaga rename<cr>', opts)
--- .config/nvim/lua/michaelballos/maps.lua
+++ .config/nvim/lua/michaelballos/maps.lua
@@ -4,19 +4,42 @@ local map = require('utils').map
 -- leader key
 vim.g.mapleader = ';'
 
--- format
-map('n', '<leader>;', '<cmd>lua vim.lsp.buf.formatting()<CR>', { noremap = true, silent = true })
+-- map opts
+local silent = {
+  silent = true
+}
 
--- switch terminal panes
-map('n', '<C-j>', '<C-w>j', { silent = true })
-map('n', '<C-k>', '<C-w>k', { silent = true })
-map('n', '<C-h>', '<C-w>h', { silent = true })
-map('n', '<C-l>', '<C-w>l', { silent = true })
+--$ format - i usually dont use formatters just vims '=' sign to fix indents
+map('n', '<leader>;', '<cmd>lua vim.lsp.buf.formatting()<CR>', silent)
 
--- vim diagnostics lsp
-map('n', '<space>e', vim.diagnostic.open_float, { silent = true })
-map('n', '[d', vim.diagnostic.goto_prev, { silent = true })
-map('n', ']d', vim.diagnostic.goto_next, { silent = true })
-map('n', '<space>q', vim.diagnostic.setloclist, { silent = true })
+--$ vim navigation
+-- switch panes
+map('n', '<C-j>', '<C-w>j', silent)
+map('n', '<C-k>', '<C-w>k', silent)
+map('n', '<C-h>', '<C-w>h', silent)
+map('n', '<C-l>', '<C-w>l', silent)
 
+--$ file
+-- quick close file no save
+map('n', '<leader>q', '<cmd>:q<CR>', silent)
+--- quick save file
+map('n', '<leader>w', '<cmd>:w<CR>', silent)
+-- new vertical split
+map('n', '<C-t>', '<cmd>:vnew<CR>', silent)
+-- new buffer
+map('n', '<C-n>', '<cmd>:enew<CR>', silent)
+
+--$ diagnostics
+-- lsp
+map('n', '<space>e', vim.diagnostic.open_float, silent)
+map('n', '[d', vim.diagnostic.goto_prev, silent)
+map('n', ']d', vim.diagnostic.goto_next, silent)
+map('n', '<space>q', vim.diagnostic.setloclist, silent)
+-- lspsaga
+map('n', '<C-j>', '<Cmd>Lspsaga diagnostic_jump_next<cr>', silent)
+map('n', 'gh', '<Cmd>Lspsaga hover_doc<cr>', silent)
+map('n', 'gd', '<Cmd>Lspsaga lsp_finder<cr>', silent)
+map('i', '<C-k>', '<Cmd>Lspsaga signature_help<cr>', silent)
+map('n', 'gp', '<Cmd>Lspsaga preview_definition<cr>', silent)
+map('n', 'gr', '<Cmd>Lspsaga rename<cr>', silent)
 
