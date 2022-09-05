93f862e12ad66d77b830412cd36e1e8a88b69f06
michaelballos <ballos.michael@gmail.com>
2022-09-04 21:50:10 -0500

    Add pane switch mappings

--- .config/nvim/lua/michaelballos/maps.lua
+++ .config/nvim/lua/michaelballos/maps.lua
@@ -7,3 +7,10 @@ vim.g.mapleader = ';'
 -- format
 map('n', '<leader>.', '<cmd>lua vim.lsp.buf.formatting()<CR>', { noremap = true, silent = true })
 
+-- switch panes
+map('n', '<leader>j', '<C-w>j', { silent = true })
+map('n', '<leader>k', '<C-w>k', { silent = true })
+map('n', '<leader>h', '<C-w>h', { silent = true })
+map('n', '<leader>l', '<C-w>l', { silent = true })
+
+
